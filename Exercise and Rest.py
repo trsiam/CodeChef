90384c06b665539233a249ffa1879baf001687c3
Md Tohidur Rahaman Siam <trsiam3468@gmail.com>
2025-12-03 21:39:12 +0600

    Add files via upload

---
+++ Exercise and Rest.py
@@ -0,0 +1,8 @@
+#https://www.codechef.com/problems/EXREST
+"""Chef is on a new exercise plan: he'll work out for two days, then take one rest day, and then repeat.
+
+Today is Chef's 
+N
+N-th rest day. How many days (including today) has it been since Chef started his plan?
+"""
+
